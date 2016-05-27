942e78160c8bcdb2620708477540b21c18e08b75
Sara <given.sara@gmail.com>
2016-05-27 14:10:27 -0700

    syling of the header area to include the search button

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -35,9 +35,12 @@
 					<nav id="site-navigation" class="main-navigation" role="navigation">
 						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						<div class="header-search">
+								<?php get_search_form(); ?>
+						</div>
 					</nav><!-- #site-navigation -->
 
-					<?php get_search_form(); ?>
+
 				</div>
 			</header><!-- #masthead -->
 
