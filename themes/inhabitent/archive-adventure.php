6875338f9e70d8a9d8493780274cee15080ad31c
Sara <given.sara@gmail.com>
2016-05-26 17:21:45 -0700

    markup and layout for adventure archive and single pages

---
+++ themes/inhabitent/archive-adventure.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying archive of Adventure Custom Post Type.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main container" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="cpt-archive-header">
+				<h1>Latest Adventures</h1>
+
+			</header><!-- .page-header -->
+				<div class="cpt-grid adventure-grid">
+    			<?php /* Start the Loop */ ?>
+    			<?php while ( have_posts() ) : the_post(); ?>
+
+  					<div class="adventure-post">
+
+              <div class="adventure-wrapper">
+                <a href="<?php the_permalink(); ?>">
+    							<?php if ( has_post_thumbnail() ) : ?>
+    								<?php the_post_thumbnail( 'large' ); ?>
+    							<?php endif; ?>
+                </a>
+    						  <div class="adventure-info">
+      							<?php the_title( sprintf( '<h2 class="adventure-name"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+                    <a href="<?php the_permalink(); ?>" class="button transparent">Read More</a>
+  						    </div>
+
+              </div> <!-- end adventure-wrapper -->
+
+  					</div><!-- end adventure-post -->
+
+			    <?php endwhile; ?>
+      </div> <!-- end adventure-grid -->
+
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ themes/inhabitent/single-adventure.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying all single adventure posts.
+ *
+ * @package Inhabitent_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="adventure-header">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<?php the_post_thumbnail( 'large' ); ?>
+					<?php endif; ?>
+				</div>
+
+				<div class="adventure-container">
+					<?php the_title( '<h1 class="adventure-title">', '</h1>' ); ?>
+          <div class="entry-meta">
+            <?php red_starter_posted_by(); ?>
+          </div>
+					<div class="entry-content">
+						<h3 class="price"><?php echo CFS()->get( 'price' ); ?></h3>
+	      		<?php the_content(); ?>
+
+	      	</div><!-- .entry-content -->
+
+					<div class="social-buttons">
+						<a href="#" class="social-button">Like</a>
+						<a href="#" class="social-button">Tweet</a>
+						<a href="#" class="social-button">Pin</a>
+					</div>
+				</div>
+
+      </article><!-- #post-## -->
+
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
