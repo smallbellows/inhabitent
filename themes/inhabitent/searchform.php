c75b9cab02b60b835a24503d790af1d67169955d
Sara <given.sara@gmail.com>
2016-06-06 14:42:27 -0700

    styling of 404 page

--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
 		<label>
-			<input type="search" class="search-field header" placeholder="TYPE AND HIT ENTER" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input type="search" class="search-field header" placeholder="Type and Hit Enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 	</fieldset>
 
